c43505161adade46a17a051b068d33c962904f10
Lorenzo Paiola <lorenzopaiola@protonmail.com>
2024-10-09 20:17:24 +0200

    ModelingToolkit maledetto, mollato lì. Ora con DIffEq funziona più o meno.

--- scripts/fILC_Santina_Angelini.jl
+++ scripts/fILC_Santina_Angelini.jl
@@ -1,14 +1,14 @@
-using DrWatson, ModelingToolkit, OrdinaryDiffEq, GLMakie, Latexify, Symbolics, LinearAlgebra, Integrals
+using DrWatson, ModelingToolkit, OrdinaryDiffEq, Symbolics, LinearAlgebra, Integrals
 using ModelingToolkit: t_nounits as t, D_nounits as D   #you can define these yourself, but included
 
-include("../src/functions.jl")
+includet("../src/functions.jl")
 
 ## System Parameters
 N = 5
 o = 3
 x₀ = zeros(N)
 v₀ = zeros(N)
-ps = @parameters m κ σ α[1:2N*o] # mass, stiffness, damping
+ps = @parameters m κ σ α[1:2N,1:o] # mass, stiffness, damping
 vars = @variables x(t)[1:N], v(t)[1:N]
 inputs = @variables u(t) [input=true]
 outputs = @variables y(t)[1:2N] [output=true]
@@ -24,7 +24,7 @@ ywant = vcat(y₁,y₂,y₃)
 
 ## Parameters
 T = [8. 18. 20.] # first time is always zero
-p = [1, 2, 1, [zeros(2*N*o)]] # mass, stiffness, damping
+p = [1, 2, 1, [zeros(2*N,o)]] # mass, stiffness, damping
 Niter = 100
 
 ## System's Equations
@@ -34,8 +34,6 @@ eqs = [ [D(x[i]) ~ v[i] for i in eachindex(x)]          #dotx=v
         [D(v[end]) ~ κ / m * x[end-1] - κ / m * x[end] + σ / m * v[end-1] - σ / m * v[end] + u / m]
         [y[i] ~ x[i] for i in 1:N]                      #y=x
         [y[i] ~ v[i-N] for i in N+1:2N]]                
-#append!(eqs, eqs2, eqs3, eqs4, eqsoutstate, eqsoutvel)
-
 ## Define System without input policy and obtain jacobian/state matrices (just because I'm lazy)
 @named sysnoinput = ODESystem(eqs, t)
 J = calculate_jacobian(sysnoinput)
@@ -48,71 +46,48 @@ An = Float64.(substitute(A, Dict(ps.=> p)))
 Bn = Float64.(substitute(B, Dict(ps.=> p)))
 Cn = Float64.(substitute(C, Dict(ps.=> p)))     
 
-## functions
+##  
+# Parametrized Inputs (basis defined in src file)
 
-function expmapproxint(A,t;order=5) 
-    T = promote_type(eltype(A), typeof(t))
-    E = Matrix{T}(I,size(A)...)
-    term = E
-
-    for i in 1:order 
-        term *= A*t/i 
-        E += simplify(term)
-    end
-
-    return simplify(E)
+function utotn(α::AbstractVector{<:Num}, s::Num) ::Num
+    return uiter(An, Bn, Cn, T, s, α)
 end
 
-expmapprox(A::AbstractMatrix{Num},t::Real) = expmapproxint(A,t)
-expmapprox(A::AbstractMatrix{Real},t::Num) = expmapproxint(A,t)
-expmapprox(A::AbstractMatrix{Num},t::Num) = expmapproxint(A,t)
-expmapprox(A,t) = exp(A*t)
-
-## Function Basis
-function CAtB(A,B,C,t)
-    return C * expmapprox(A,t) * B
-end
-CAtB(A::AbstractMatrix{T},B::AbstractMatrix{T},C::AbstractMatrix{T},t::T) where T<:AbstractFloat = C*exp(A*t)*B
-
-function pif(A,B,C,T,i,t)
-    minT = i==1 ? 0 : T[i-1]
-    maxT = T[i]
-    return ifelse(!((t>maxT)&(t<minT)), CAtB(A,B,C,maxT-t)', zeros(size(B,2),size(C,1)))
-    #return ifelse(!((t>T[i])&(t<T[i-1])), B' * (exponential(A'*(T[i]-t))) * C', zeros(size(B,2),size(C,1)))
-end
-
-
-# Parametrized inputs
-# Each elements (o=3) is a function which outputs a vector of size l×m = 1x2N
-piarr = [s->pif(A,B,C,T,i,s) for i in eachindex(T)]
-piarrn = [s->pif(An,Bn,Cn,T,i,s) for i in eachindex(T)]
-piall(s) = [piarr[i](s) for i in eachindex(piarr)]
-pitot(s)  = vcat(piall(s)...)
-pitotn(s) = hcat(hcat,[piarrn[i](s) for i in eachindex(piarrn)]...)
-
-#utot(α,s) = mapreduce(x->dot(x[1](s),x[2]),+,zip(piarr,collect(eachcol(reshape(α,2N,o)))))
-#utotn(α,s) = mapreduce(x->dot(x[1](s),x[2]),+,zip(piarrn,collect(eachcol(reshape(α,2N,o)))))
-#
-
-utot(α,s) = pitot(s)*α[:]
-utotn(α,s) = pitotn(s)*α
-@register_symbolic utot(α,s)
-##
-eqsinput = u ~ utot(α,t)
-##
-
-fint(t,p) = CAtB(p[1],p[2],p[3],t)*utot(p[4],t)
-
+@register_symbolic utotn(α,s)
+##              
+eqsinput = [u ~ utotn(α,t)]
 ## fILC
 # essentially @named but better as it simplifies stuff along the way if it can. Before we could not use it as u wasnt defined
-@mtkbuild sys = ODESystem(append!(eqs,eqsinput),t)
+#@named sys = ODESystem(push!(eqs,eqsinput),t)
 
+eqsnew = vcat(eqs,eqsinput)
+@mtkbuild sys = ODESystem(eqsnew,t)
+
+##
 xⁱ = zeros(N,o,Niter)
 vⁱ = zeros(N,o,Niter) 
 solarray = Vector{Any}(undef,Niter)
-αrray = zeros(2*N*o,Niter)
+αrray = zeros(2*N,o,Niter)
 
 method = N<=5 ? HCubatureJL() : VEGASMC()
+
+## Build H
+Twith0 = [0.0 T[:]...]
+ftoint1 = [i>j ? ((t,p)-> Cn*exp(An*(Twith0[i]-t))*Bn*Bn'* exp(An'*(Twith0[j]-t))*Cn') : zeros(2N,2N)
+            for i in 2:o+1, j in 1:o]
+
+# subdivide in multiple integrals so that it's easier (pis have bounded domain)
+H = zeros(2N*o,2N*o)
+for i in 1:o, j in 1:o 
+    blockrangei = (i-1)*2N+1:i*2N
+    blockrangej = (j-1)*2N+1:j*2N
+
+    H[blockrangei,blockrangej] .= i>=j ? solve(IntegralProblem(ftoint1[i,j],(Twith0[j],Twith0[j+1])),method).u[:,:] : ftoint1[i,j]
+end
+
+L = inv(H'H+I(2N*o)*1e-2)*H'
+
+##
 
 for i in 1:Niter
     # Instantiate the symbolic System
@@ -122,11 +97,10 @@ for i in 1:Niter
                             ps.=>p) #parameters (includes different α)
     sol = solve(systosolve, Tsit5())
     
-    Hprob = [IntegralProblem(fint,(0,T[i]),(An,Bn,Cn,p[end])) for i in 2:o+1]
     H = vcat([solve(Hprob[i],method) for i in eachindex(Hprob)]...)
     solarray[i] = sol
     xⁱ[:,:,i] = sol[x](T)
     vⁱ[:,:,i] = sol[v](T)
-    αrray[:,i+1] = αrray[:,i] + pinv(H)*([ywant - hcat(xⁱ[:,:,i],yⁱ[:,:,i])[:]])    
-    p[end] = αrray[:,i+1]
+    αrray[:,:,i+1] = αrray[:,:,i] + reshape(L*([ywant - hcat(xⁱ[:,:,i],yⁱ[:,:,i])[:]]),2N,o)    
+    p[end] = αrray[:,:,i+1]
 end
--- src/functions.jl
+++ src/functions.jl
@@ -1,48 +1,56 @@
-function createsystem()
-    @parameters m κ σ  # mass, stiffness, damping
-    ps = [m;κ;σ]
-    @variables x(t)[1:N] v(t)[1:N] u(t)
+    # Defined just because exp does not work with Matrix{Num}
+    function expmapproxint(A,t;order=5) 
+        T = promote_type(eltype(A), typeof(t))
+        E = Matrix{T}(I,size(A)...)
+        term = E
 
-    ### System's Equations
-    eqs1 = [D(x[i]) ~ v[i] for i in eachindex(x)]
-    eqs2 = [D(v[1]) ~ -2κ / m * x[1] + κ / m * x[2]]
-    eqs3 = [
-        D(v[i]) ~ κ / m * x[i-1] - 2κ / m * x[i] + κ / m * x[i+1] + σ / m * v[i-1] - 2σ / m * v[i] + σ / m * v[i+1] for i in 2:length(x)-1
-    ]
-    eqs4 = [D(v[end]) ~ κ / m * x[end-1] - κ / m * x[end] + σ / m * v[end-1] - σ / m * v[end] + u / m]
-    append!(eqs1, eqs2, eqs3, eqs4)
+        for i in 1:order 
+            term *= A*t/i 
+            E += simplify(term)
+        end
 
-    return ps, ODESystem(eqs1, t)
-end
-
-# Defined just because exp does not work with Matrix{Num}
-function expmapproxint(A,t;order=5) 
-    T = promote_type(eltype(A), typeof(t))
-    E = Matrix{T}(I,size(A)...)
-    term = E
-
-    for i in 1:order 
-        term *= A*t/i 
-        E += simplify(term)
+        return simplify(E)
     end
 
-    return simplify(E)
-end
+    function expnumsymb(A::AbstractMatrix{T},t::Num) where T 
+        evals, evec = eigen(A)
+        return evec * diagm(exp.(evals.*t)) * evec^-1
+    end
 
-expmapprox(A::AbstractMatrix{Num},t::Real) = expmapproxint(A,t)
-expmapprox(A::AbstractMatrix{Real},t::Num) = expmapproxint(A,t)
-expmapprox(A::AbstractMatrix{Num},t::Num) = expmapproxint(A,t)
-expmapprox(A,t) = exp(A*t)
 
-## Function Basis
-function CAtB(A,B,C,t)
-    return C * expmapprox(A,t) * B
-end
-CAtB(A::AbstractMatrix{T},B::AbstractMatrix{T},C::AbstractMatrix{T},t::T) where T<:AbstractFloat = C*exp(A*t)*B
+    expsym(A::AbstractMatrix{Num},t::T) where T = expmapproxint(A,t)
+    expsym(A::AbstractMatrix{T},t::Num) where T = expnumsymb(A,t)
+    expsym(A::AbstractMatrix{Num},t::Num) = expmapproxint(A,t)
+    expsym(A::AbstractMatrix{T},t::T) where T = exp(A*t) 
 
-function pif(A,B,C,T,i,t)
-    minT = i==1 ? 0 : T[i-1]
-    maxT = T[i]
-    return ifelse(!((t>maxT)&(t<minT)), CAtB(A,B,C,maxT-t)', zeros(size(B,2),size(C,1)))
-    #return ifelse(!((t>T[i])&(t<T[i-1])), B' * (exponential(A'*(T[i]-t))) * C', zeros(size(B,2),size(C,1)))
-end
+    ## Function Basis
+    function CAtB(A,B,C,t)
+        return C * expsym(A,t) * B
+    end
+    CAtB(A::AbstractMatrix{T},B::AbstractMatrix{T},C::AbstractMatrix{T},t::T) where T = C*exp(A*t)*B
+
+    function pif(A,B,C,T,i,t)
+        minT = i==1 ? 0 : T[i-1]
+        maxT = T[i]
+        return ifelse(!((t>maxT)&(t<minT)), CAtB(A,B,C,maxT-t)', zeros(size(B,2),size(C,1)))
+        #return ifelse(!((t>T[i])&(t<T[i-1])), B' * (exponential(A'*(T[i]-t))) * C', zeros(size(B,2),size(C,1)))
+    end
+
+    function piall(A,B,C,T,t)
+
+        minT = [0.0 T[1:end-1]...]
+        return hcat(ifelse(!((t>T[i])&(t<minT[i])), CAtB(A,B,C,T[i]-t), 0.0) for i in eachindex(T))
+
+    end
+
+    function uiter(A,B,C,T,t,α)
+        minT= [0.0 T[1:end-1]...]       
+        return sum(ifelse(!((t>T[i])&(t<minT[i])), dot(CAtB(A,B,C,T[i]-t),α[:,i]), 0.0) for i in eachindex(T))
+    end
+
+    function cleanupimaginary(arraytoclean::AbstractVecOrMat{Num};tol=1e-8)
+        indexes = map(x->Symbolics.coeff(arraytoclean))
+    end
+
+    function cleanupimaginary(arraytoclean::AbstractVecOrMat{T};tol=1e-8) where T
+    end
